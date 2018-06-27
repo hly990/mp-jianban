11e9fc8c9dc1eedee4f610b364c24c732599c851
He <hsl990@163.com>
2018-06-27 09:51:24 +0800

    commit jbindex 2

---
+++ src/pages/jbindex/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container p-0 bg-light" style="min-height: 100vh">
+    <div class="row  p-2 justify-content-center align-items-center">
+      <a class="d-block btn text-white" href="#" role="button" style="background-color: #D78F30">
+        Start a New Ventures
+      </a>
+    </div>
+    <div class="row hidden p-2 justify-content-center align-items-center">
+      <a class="d-block btn text-white" href="#" role="button" style="background-color: #D78F30">
+        Start a New Ventures
+      </a>
+    </div>
+    <div class="row hidden p-2 justify-content-center align-items-center">
+      <a class="d-block btn text-white" href="#" role="button" style="background-color: #D78F30">
+        Start a New Ventures
+      </a>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+
+</script>
+
+<style>
+
+
+</style>
